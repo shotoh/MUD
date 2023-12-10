48746017f1e1c4f4a1e57a4d0112cdd13985e29b
shotoh <trueshotoh@gmail.com>
2023-12-09 20:20:09 -0800

    added mqtt payload publishing

--- mud_client.c
+++ mud_client.c
@@ -9,33 +9,31 @@
 #define PORT 8888
 
 int main(int argc, char const* argv[]) { //
-    int socketfd;
+    int sockfd;
     struct sockaddr_in address;
     char buffer[MAX_LEN];
 
-    if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("Socket creation failed");
         exit(EXIT_FAILURE);
     }
     memset(&address, 0, sizeof(address));
+    memset(buffer, 0, sizeof(buffer));
     address.sin_family = AF_INET;
     address.sin_port = htons(PORT);
     address.sin_addr.s_addr = inet_addr("35.212.252.59");
 
-    if (connect(socketfd, (struct sockaddr*) &address,sizeof(address)) < 0) {
+    if (connect(sockfd, (struct sockaddr*) &address,sizeof(address)) < 0) {
         printf("Connection Failed");
         exit(EXIT_FAILURE);
     }
 
+    while (1) {
+        fgets(buffer, MAX_LEN, stdin);
+        write(sockfd, buffer, strlen(buffer));
+        if (!strncmp(buffer, "exit", strlen("exit"))) break;
+    }
 
-
-    write(socketfd, "Hello", strlen("Hello"));
-    printf("Hello message sent\n");
-    read(socketfd, buffer, MAX_LEN - 1);
-    printf("%s\n", buffer);
-
-
-
-    close(socketfd);
+    close(sockfd);
     return 0;
 }
