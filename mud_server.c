3dd96de65cd61fae87879078165339adadd46355
shotoh <trueshotoh@gmail.com>
2023-12-09 23:14:45 -0800

    remove unused swap helper func

--- mud_server.c
+++ mud_server.c
@@ -136,14 +136,6 @@ int empty() {
     return (top == -1) ? 1 : 0;
 }
 
-void swap(char *str1, char *str2) {
-    char *temp = (char *) malloc((strlen(str1) + 1) * sizeof(char));
-    strcpy(temp, str1);
-    strcpy(str1, str2);
-    strcpy(str2, temp);
-    free(temp);
-}
-
 void payload(char* payload) {
     int rc; // response code
     MQTTClient_message msg = MQTTClient_message_initializer;
